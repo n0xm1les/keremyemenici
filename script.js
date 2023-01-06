375b8c1e48f258b40c8d8bdd87ff10e0909c3863
Kerem Yemenici <nxmles@gmail.com>
2023-01-06 21:09:21 +0300

    Created about me section

--- script.js
+++ script.js
@@ -5,15 +5,50 @@ const frontMountains = document.querySelector("img#front-mountains");
 const trees = document.querySelector("img#trees");
 const firstSee = document.querySelector("div#first-see");
 const navbar = document.querySelector("nav#navbar");
-console.log(firstSee);
+const slidingText = document.querySelector(".sliding-text")
+var playedAnimations = []
 
-window.addEventListener("scroll", function (e) {
+window.addEventListener("scroll", scrolling)
+
+function scrolling(e) {
     let value = window.scrollY;
     clouds.style.top = value * 0.5 + "px";
     clouds.style.left = value * 0.7 + "px";
     backMountains.style.top = value * 0.5 + "px";
     middleMountains.style.top = value * 0.35 + "px";
-    frontMountains.style.top = value * 0.1 + "px";
+    frontMountains.style.top = value * 0.2 + "px";
     firstSee.style.top = value * 0.48 + "px";
     navbar.style.top = value + "px";
-})
+    if (value>=300 && !playedAnimations.includes("about-me-title-slide")) {
+        slideAboutMeTitle.play()
+        playedAnimations.push("about-me-title-slide")
+    }
+
+    if (value>=700 && !playedAnimations.includes("about-me-item-slide")) {
+        slideAboutMeContent.play()
+        playedAnimations.push("about-me-item-slide")
+    }
+}
+
+var slideAboutMeTitle = anime({
+    targets: '#about-me-title',
+    translateX: "50%",
+    opacity: 0,
+    direction: "reverse",
+    autoplay: false,
+    duration: 2000,
+    easing: "easeInBack",
+});
+
+var slideAboutMeContent = anime({
+    targets: '.about-me-item',
+    translateX: "50%",
+    opacity: 0,
+    direction: "reverse",
+    autoplay: false,
+    duration: 2000,
+    delay: anime.stagger(500),
+    easing: "easeInCubic",
+});
+
+scrolling()
